a4d1597d6fbb932aa598f960448ff5aaae10c49a
Eduardo <eduangelinicarvalho@gmail.com>
2023-08-03 16:13:55 -0300

    commit#3-Header and search

---
+++ src/pages/cardapio/Buscador/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './Buscador.module.scss'
+import { CgSearch } from 'react-icons/cg'
+
+interface BuscadorProps {
+  busca: string
+  setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Buscador = ({ busca, setBusca }: BuscadorProps) => {
+  return (
+    <div className={styles.buscador}>
+      <input
+        value={busca}
+        onChange={evento => setBusca(evento.target.value)}
+      />
+      <CgSearch
+        size={20}
+        color='#4c4d5e'
+      />
+    </div>
+  )
+}
+
+export default Buscador
